66364d466a49a02ed90d2b4df9cb35cb528f0779
Aamir <113689022+aliamirr@users.noreply.github.com>
2022-11-05 07:47:45 +0530

    Update PalindromePattern.java

--- src/PalindromePattern.java
+++ src/PalindromePattern.java
@@ -5,16 +5,16 @@ public class PalindromePattern {
 
         for (int i = 1; i <=m ; i++) {
             //Spaces
-            for (int j = 1; j < m-i; j++) {
-                System.out.print(" ");
+            for (int j = 1; j <= m-i; j++) {
+                System.out.print("  ");
             }
             //1st Half Numbers
             for (int j = i; j >= 1; j--) {
-                System.out.print(j);
+                System.out.print(j + " ");
             }
             //2nd Half Numbers
             for (int j = 2  ; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(j + " ");
             }
             System.out.println();
         }
